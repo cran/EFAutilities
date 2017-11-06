fdb1b6c78109640c871a0c25dd1e1f5c22f74592
Guangjian Zhang <gzhang3@nd.edu>
2017-11-06 19:04:50 +0000

    version 1.1.1

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,5 @@
 # EFAutilities 1.1.0
-
+-Third release fixed bug in Align.Matrix
 -Second release with efaMR, Align.Matrix functions
 -First release
 
--- R/Alignment20171102_c.R
+++ R/Alignment20171102_c.R
@@ -1,16 +1,16 @@
 # Align.Matrix is an R function to reflect and interchange columns of Input.Matrix to match
-# those of Order.Matrix. Becuase it considers all possible permutations of columns of Input.Matrix,
-# the best match in terms of the smallest sum of squared deviations between these two matrices can always be found.
+# those of Order.Matrix. The function first uses a quick method. If the quick method fails to find the
+# the best match, the function goes through all possible permutations of columns of Input.Matrix.
+# The best match in terms of the smallest sum of squared deviations between these two matrices can always be found.
 # It works well when the number of factors is not too large.
-# Updated on 2016-06-16 to include an internal function fn_perm_list, Guangjian Zhang
-# Updated on 2016-06-30 to accommodate partially specified order matrices, Guangjian Zhang
-
 
 # 
 # Order.Matrix, an input argument, p by m
 # Input.Matrix, an input argument, (p+m) by m, the first p rows are factor loadings, the last m rows are factor correlations
-# Output.Matrix, an output argument, (p+m+1) by m, the last row contains the sums of squared deviations for the best match
-# and the second best match. The difference between the best match and the second best match could be considered as a confidence
+# Output.Matrix, an output argument, (p+m+1) by m, the first p rows are factor loadings, the next m rows are factor correlations
+#                                                  the last row contains the sums of squared deviations for the best match
+#                                                 and the second best match (if the quick method fails). 
+# The difference between the best match and the second best match could be considered as a confidence
 # on the success of the aligning procedure.
 
 
@@ -38,8 +38,7 @@ m = ncol(Order.Matrix)
 
 if (is.null(Weight.Matrix)) Weight.Matrix = matrix(1,p,m)
 
-Permutation = fn_perm_list(m,m)
-Factorial.m = nrow(Permutation)
+Factorial.m = factorial(m)
 
 Loading.Matrix = Input.Matrix[1:p,1:m]
 Phi.Matrix = Input.Matrix[(p+1):(p+m),1:m]
@@ -49,57 +48,76 @@ Phi.Matrix = Input.Matrix[(p+1):(p+m),1:m]
 # Rows correspond to Order.Matrix
 # Columns Correspond to Input.Matrix
 
-Squared.Deviation = diag(m)
+Squared.Deviation = matrix(0,m,m)
+Sign.Matrix = matrix(1,m,m)
 
-Order.Matrix.abs = abs(Order.Matrix)
-Loading.Matrix.abs = abs(Loading.Matrix)
+# Order.Matrix.abs = abs(Order.Matrix)
+# Loading.Matrix.abs = abs(Loading.Matrix)
+
 
 for (i in 1:m) {
-    temp1= sum(Order.Matrix.abs[1:p,i] * Order.Matrix.abs[1:p,i] * Weight.Matrix[1:p,i])
+    temp1= sum(Order.Matrix[1:p,i] * Order.Matrix[1:p,i] * Weight.Matrix[1:p,i])
    for (j in 1:m) {
-    temp2 = sum(Loading.Matrix.abs[1:p,j] * Loading.Matrix.abs[1:p,j] * Weight.Matrix[1:p,i])
-    temp3 = sum(Order.Matrix.abs[1:p,i] * Loading.Matrix.abs[1:p,j]  * Weight.Matrix[1:p,i])
-    Squared.Deviation[i,j] = temp1 + temp2 - 2 * abs(temp3)  
-                 } # (j in 1:m)
+    temp2 = sum(Loading.Matrix[1:p,j] * Loading.Matrix[1:p,j] * Weight.Matrix[1:p,i])
+    temp3 = sum(Order.Matrix[1:p,i] * Loading.Matrix[1:p,j]  * Weight.Matrix[1:p,i])
+      if(temp3<0) Sign.Matrix[i,j] = -1
+      Squared.Deviation[i,j] = temp1 + temp2 - 2 * abs(temp3)  
+                       } # (j in 1:m)
               } # (i in 1:m)
 
+# Step 2, a quick and a dirty method
+Quick.Match = rep(0,m)
+Quick.Squared.Deviation = 0
+for (i in 1:m) {
+  Quick.Match[i] = order(Squared.Deviation[i,1:m])[1]
+  Quick.Squared.Deviation = Quick.Squared.Deviation + Squared.Deviation[i,Quick.Match[i]]
+  } # (i in 1:m)
 
-# Step 2, find the best match betwen the Order.Matrix and Loading.Matrix
+
+# Step 3, find the best match betwen the Order.Matrix and Loading.Matrix
+
+if (prod(Quick.Match) == Factorial.m ) {
+  Final.Match = Quick.Match
+} else {
 
 Squared.Deviation.Permutation = rep(0,Factorial.m)
+Permutation = fn_perm_list(m,m)
+
 for (i in 1:Factorial.m) {
 for (j in 1:m) {
  Squared.Deviation.Permutation[i] = Squared.Deviation.Permutation[i] + Squared.Deviation[j,Permutation[i,j]]
 }
 }
 
-
 Match = order(Squared.Deviation.Permutation)
 
+Final.Match = Permutation[Match[1],1:m]
 
-# step 3, Interchange columns of the factor loading matrix to match the target factor loading matrix
+} ## (prod(Quick.Match) == factorial(m))
+
+# step 4, Interchange columns of the factor loading matrix to match the target factor loading matrix
 
 Temp.Loading.Matrix = Loading.Matrix
 Temp.Phi.Matrix = Phi.Matrix
 
 for (i in 1:m) {
-Temp.Loading.Matrix[1:p,i] = Loading.Matrix[1:p,Permutation[Match[1],i]]
+Temp.Loading.Matrix[1:p,i] = Loading.Matrix[1:p,Final.Match[i]]
 }
 
  for (i in 1:m) {
   for (j in 1:m) {
-   Temp.Phi.Matrix[i,j] = Phi.Matrix[Permutation[Match[1],i],Permutation[Match[1],j]]   
+   Temp.Phi.Matrix[i,j] = Phi.Matrix[Final.Match[i],Final.Match[j]]   
  }
 }
 
 
-## Step 4, reflect columns of Phi.Matrix if needed
+## Step 5, reflect columns of Phi.Matrix if needed
 
 for (j in 1:m) {
 
-temp1 = sum (Order.Matrix[1:p,j] * Temp.Loading.Matrix[1:p,j])
+# temp1 = sum (Order.Matrix[1:p,j] * Temp.Loading.Matrix[1:p,j])
 
- if (temp1 < 0) {
+ if (Sign.Matrix[j,Final.Match[j]] < 0) {
  Temp.Loading.Matrix[1:p,j] = Temp.Loading.Matrix[1:p,j] * (-1)
  Temp.Phi.Matrix[j,1:m] = Temp.Phi.Matrix[j,1:m] * (-1)
  Temp.Phi.Matrix[1:m,j] = Temp.Phi.Matrix[1:m,j] * (-1) 
@@ -111,8 +129,16 @@ temp1 = sum (Order.Matrix[1:p,j] * Temp.Loading.Matrix[1:p,j])
 Output.Matrix = array (rep(0,(p+m+1)*m), dim=c((p+m+1),m))
 Output.Matrix[1:p,1:m] = Temp.Loading.Matrix[1:p,1:m]
 Output.Matrix[(p+1):(p+m),1:m] = Temp.Phi.Matrix[1:m,1:m]
+
+
+if (prod(Quick.Match) == Factorial.m ) {
+  Output.Matrix[(p+m+1),1] = Quick.Squared.Deviation
+  Output.Matrix[(p+m+1),2] = 0
+} else {
 Output.Matrix[(p+m+1),1] = Squared.Deviation.Permutation[Match[1]]
 Output.Matrix[(p+m+1),2] = Squared.Deviation.Permutation[Match[2]]
+} # if (prod(Quick.Match) == factorial(m))
+
 
 Output.Matrix
 
